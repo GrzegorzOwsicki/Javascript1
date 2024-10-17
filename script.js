386b6de0a9ad4272c7b8406eeeaf7ddd952a750e
GrzegorzOwsicki <117662448+GrzegorzOwsicki@users.noreply.github.com>
2024-10-17 09:57:58 +0200

    Cm2_2
    skończone

--- script.js
+++ script.js
@@ -15,9 +15,32 @@
       })
   })
 
+  const showLoadingPopup = () => {
+    const loadingPopup = document.createElement('div');
+    loadingPopup.id = 'loadingPopup';
+ 
+    loadingPopup.innerText = 'Loading…';
+    loadingPopup.style.position = 'fixed';
+    loadingPopup.style.top = '50%';
+    loadingPopup.style.left = '50%';
+    loadingPopup.style.transform = 'translate(-50%, -50%)';
+    loadingPopup.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+    loadingPopup.style.color = 'white';
+    loadingPopup.style.padding = '20px';
+    loadingPopup.style.borderRadius = '5px';
+    document.body.appendChild(loadingPopup);
+  };
+
+  const hideLoadingPopup = () => {
+    const loadingPopup = document.getElementById('loadingPopup');
+    if (loadingPopup) {
+      loadingPopup.remove();
+    }
+  };
+
   cw1.addEventListener("click", function () {
     //TODO
-    answer.innerHTML=('loading')
+    showLoadingPopup();
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(posts => {
@@ -29,7 +52,7 @@
         const li = document.createElement('li');
         li.innerHTML = `<strong> User : ${post.id} ${post.title} </strong><br> ${post.body}`;
         ul.appendChild(li);
-        
+        hideLoadingPopup();
       });
       answer.appendChild(ul)
       
@@ -38,7 +61,7 @@
 
   cw2.addEventListener("click", function () {
     //TODO
-    answer.innerHTML=('Loading..');
+    showLoadingPopup();
 
     fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then(response => response.json())
@@ -47,13 +70,14 @@
       answer.innerHTML='';
       const postDiv = document.createElement('div')
       postDiv.innerHTML = `<strong>${post.title}</strong>${post.body}`;
-
+      hideLoadingPopup();
       answer.appendChild(postDiv);
     });
   });
 
   cw3.addEventListener("click", function () {
     //TODO
+    showLoadingPopup();
     const createPost = {
       title : 'new Post',
       body : 'lorem ipsum',
@@ -71,6 +95,7 @@
     .then(response => response.json())
     .then(data => {
       console.log(data)
+      hideLoadingPopup();
       answer.innerHTML=`Dodano nowy post o nazwie = ${data.id}`;
       postDiv.innerHTML = `<strong>${post.title}</strong>${post.body}`;
     });
