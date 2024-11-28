dbf1a60a6829f5442741e9a809c7f1a3c724c4f9
GrzegorzOwsicki <117662448+GrzegorzOwsicki@users.noreply.github.com>
2024-11-28 09:12:14 +0100

    Cm4_1.2.

--- index.js
+++ index.js
@@ -22,6 +22,26 @@ app.get('/math/circle/:r', (req, res) => {
     res.json(result);
   });
 
+
+  app.get('/math/rectangle/:width/:heigh', (req,res) => {
+    const a = parseFloat(req.params.width);
+    const h = parseFloat(req.params.heigh);
+  
+    if(isNaN(a) || a <= 0 || isNaN(h) || h <= 0){
+      return res.status(400).json({ error: 'Invalid input' });
+    }
+  
+    const pole = a * h;
+    const obwod = 2*a+2*h;
+  
+    const result = {
+      pole: pole,
+      obwod: obwod
+      
+    }
+  
+    res.json(result);
+  })
 // start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
